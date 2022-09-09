827b5998f2ba72d7399c707cf2bd723e4084bdb3
Ami-OS <grizzltty.app@gmail.com>
2022-09-09 12:55:46 +0800

    Add github pages deploy

---
+++ gh-pages.js
@@ -0,0 +1,22 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+  './',
+  {
+    src: [
+      'docs/**/*',
+      'dist/**/*',
+      '.nojekyll',
+      'node_modules/video.js/dist/*',
+      'node_modules/videojs-contrib-quality-levels/dist/*',
+      'node_modules/@samueleastdev/videojs-dash-hls-bitrate-switcher/dist/*',
+      'node_modules/@samueleastdev/videojs-settings-menu/dist/*'
+    ],
+    dotfiles: true,
+    repo: 'https://github.com/paidless/videojs-yt-style.git'
+  },
+  function(err) {
+    // eslint-disable-next-line
+    console.log(err);
+  }
+);
