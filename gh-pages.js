c475f6f4c7369f8add83149dceed275d23b8a63a
Ami-OS <grizzltty.app@gmail.com>
2022-09-09 13:42:49 +0800

    Fix load moblie ui

--- gh-pages.js
+++ gh-pages.js
@@ -10,7 +10,8 @@ ghpages.publish(
       'node_modules/video.js/dist/*',
       'node_modules/videojs-contrib-quality-levels/dist/*',
       'node_modules/@samueleastdev/videojs-dash-hls-bitrate-switcher/dist/*',
-      'node_modules/@samueleastdev/videojs-settings-menu/dist/*'
+      'node_modules/@samueleastdev/videojs-settings-menu/dist/*',
+      'node_modules/videojs-mobile-ui/dist/*'
     ],
     dotfiles: true,
     repo: 'https://github.com/paidless/videojs-yt-style.git'
