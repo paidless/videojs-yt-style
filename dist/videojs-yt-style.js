d8044260c1bd9175859498c4b47a695b565713c4
Ami-OS <grizzltty.app@gmail.com>
2022-09-11 23:55:04 +0800

    Updates

--- dist/videojs-yt-style.js
+++ dist/videojs-yt-style.js
@@ -1,4 +1,4 @@
-/*! @name videojs-yt-style @version 0.0.0 @license UNLICENSED */
+/*! @name videojs-yt-style @version 0.1.0 @license UNLICENSED */
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('video.js')) :
 	typeof define === 'function' && define.amd ? define(['video.js'], factory) :
@@ -49,7 +49,83 @@
 	  module.exports["default"] = module.exports, module.exports.__esModule = true;
 	});
 
-	var version = "0.0.0";
+	var version = "0.1.0";
+
+	var Dom = videojs__default['default'].dom; // https://github.com/Ami-OS/video.js/blob/65750e311661e70f170e3652573caacf6f21fcce/src/js/control-bar/progress-control/time-tooltip.js#L54-L133
+
+	videojs__default['default'].getComponent('TimeTooltip').prototype.update = function update(seekBarRect, seekBarPoint, content) {
+	  var tooltipRect = Dom.findPosition(this.el_);
+	  var playerRect = Dom.getBoundingClientRect(this.player_.el());
+	  var seekBarPointPx = seekBarRect.width * seekBarPoint; // do nothing if either rect isn't available
+	  // for example, if the player isn't in the DOM for testing
+
+	  if (!playerRect || !tooltipRect) {
+	    return;
+	  } // This is the space left of the `seekBarPoint` available within the bounds
+	  // of the player. We calculate any gap between the left edge of the player
+	  // and the left edge of the `SeekBar` and add the number of pixels in the
+	  // `SeekBar` before hitting the `seekBarPoint`
+
+
+	  var spaceLeftOfPoint = seekBarRect.left - playerRect.left + seekBarPointPx; // This is the space right of the `seekBarPoint` available within the bounds
+	  // of the player. We calculate the number of pixels from the `seekBarPoint`
+	  // to the right edge of the `SeekBar` and add to that any gap between the
+	  // right edge of the `SeekBar` and the player.
+
+	  var spaceRightOfPoint = seekBarRect.width - seekBarPointPx + (playerRect.right - seekBarRect.right);
+
+	  if (this.options_.playerOptions.keepTimeTooltipInSeekBar) {
+	    // This is the space right of the `seekBarPoint` in the `SeekBar`
+	    spaceRightOfPoint = seekBarRect.width - seekBarPointPx; // This is the space left of the `seekBarPoint` in the `SeekBar`
+
+	    spaceLeftOfPoint = seekBarRect.width - spaceRightOfPoint;
+	  } // This is the number of pixels by which the tooltip will need to be pulled
+	  // further to the right to center it over the `seekBarPoint`.
+
+
+	  var pullTooltipBy = tooltipRect.width / 2;
+
+	  if (this.options_.playerOptions.keepTimeTooltipInSeekBar) {
+	    // The center of `seekBar`
+	    var centerPosition = seekBarRect.width / 2; // Offset value of the `centerPosition`
+
+	    var centerOffsetOfPoint = centerPosition - seekBarPointPx; // If `tooltipRect` is greater than `seekBarRect` then center the tooltip,
+	    // else patch the offset value of the tooltip overflow space.
+
+	    if (tooltipRect.width > seekBarRect.width) {
+	      pullTooltipBy += centerOffsetOfPoint;
+	    } else if (spaceLeftOfPoint < pullTooltipBy) {
+	      pullTooltipBy += pullTooltipBy - spaceLeftOfPoint;
+	    } else if (spaceRightOfPoint < pullTooltipBy) {
+	      pullTooltipBy -= pullTooltipBy - spaceRightOfPoint;
+	    }
+	  } else {
+	    // Adjust the `pullTooltipBy` distance to the left or right depending on
+	    // the results of the space calculations above.
+	    if (spaceLeftOfPoint < pullTooltipBy) {
+	      pullTooltipBy += pullTooltipBy - spaceLeftOfPoint;
+	    } else if (spaceRightOfPoint < pullTooltipBy) {
+	      pullTooltipBy = spaceRightOfPoint;
+	    } // Due to the imprecision of decimal/ratio based calculations and varying
+	    // rounding behaviors, there are cases where the spacing adjustment is off
+	    // by a pixel or two. This adds insurance to these calculations.
+
+
+	    if (pullTooltipBy < 0) {
+	      pullTooltipBy = 0;
+	    } else if (pullTooltipBy > tooltipRect.width) {
+	      pullTooltipBy = tooltipRect.width;
+	    }
+	  } // prevent small width fluctuations within 0.4px from
+	  // changing the value below.
+	  // This really helps for live to prevent the play
+	  // progress time tooltip from jittering
+
+
+	  pullTooltipBy = Math.round(pullTooltipBy);
+	  this.el_.style.right = "-" + pullTooltipBy + "px";
+	  this.write(content);
+	};
 
 	var Component = videojs__default['default'].getComponent('Component');
 
