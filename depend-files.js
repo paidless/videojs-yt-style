04ed4ddad0c04cf6ca3c29a07cd419784d83f909
Ami-OS <grizzltty.app@gmail.com>
2022-09-17 23:08:35 +0800

    Add lang files

--- depend-files.js
+++ depend-files.js
@@ -1,5 +1,6 @@
 const dependFiles = [
   'node_modules/video.js/dist/*',
+  'node_modules/video.js/dist/lang/*.js',
   'node_modules/videojs-contrib-quality-levels/dist/*',
   'node_modules/@samueleastdev/videojs-dash-hls-bitrate-switcher/dist/*',
   'node_modules/@samueleastdev/videojs-settings-menu/dist/*',
